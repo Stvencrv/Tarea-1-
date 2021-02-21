0d3245e80daa44f34e8a0db845f158d1d1dee1dc
steven caraballo <steven011004@gmail.com>
2021-02-21 15:58:49 -0400

    Categria

---
+++ TareaGit/categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaGit
+{
+    class categoria
+    {
+
+        public string Nombre { get; set; }
+
+        public categoria(string nombrecategoria)
+        {
+            this.Nombre = nombrecategoria;
+        }
+    }
+}
