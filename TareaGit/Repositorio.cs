6511259d84f92857b867e9004c969d6c2481a64a
steven caraballo <steven011004@gmail.com>
2021-02-21 15:58:21 -0400

    repositorio

---
+++ TareaGit/Repositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaGit
+{
+    class Repositorio
+    {
+        public class repositorio
+        {
+
+            public static repositorio Instancia { get; } = new repositorio();
+            public List<categoria> Litasdecategorias { get; set; } = new List<categoria>();
+            public List<producto> Listarproductos { get; set; } = new List<producto>();
+
+            private repositorio()
+            {
+
+
+            }
+        }
+}
