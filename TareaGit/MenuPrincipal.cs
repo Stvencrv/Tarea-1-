2e11e9a5bd31615d41e5981bbf8179d7e29d63e1
steven caraballo <steven011004@gmail.com>
2021-02-21 15:32:39 -0400

    Initial Commit

---
+++ TareaGit/MenuPrincipal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaGit
+{
+    public class MenuPrincipal
+    {
+
+        public void impMenu()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("1-Mantenimiento de materias\n2-Mantenimiento de estudiantes\n3-Inscripcion\n4-Salir");
+                Console.Write("Seleccion una opcion: ");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Seleccion una opcion: ");
+                        impMenu();
+                        break;
+                    case 2:
+                       
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Console.WriteLine("Gracias por utilizar nuestro sistema");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Debe seleccionar una opcion valida");
+                        Console.ReadKey();
+                        impMenu();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Debe seleccionar una opcion valida");
+                Console.ReadKey();
+                impMenu();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ TareaGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TareaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
