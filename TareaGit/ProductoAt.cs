a94cfe22082828e3ae5e0f47dcf779e71a4389d5
steven caraballo <steven011004@gmail.com>
2021-02-21 16:01:07 -0400

    Atributos de productos

---
+++ TareaGit/ProductoAt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaGit
+{
+    class ProductoAt
+    {
+        public class producto
+        {
+
+            public string Nombre { get; set; }
+
+            public double Precio { get; set; }
+
+            public producto(string nombreproducto, double precio)
+            {
+                this.Nombre = nombreproducto;
+                this.Precio = precio;
+
+            }
+
+        }
+}
